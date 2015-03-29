e43f9d82221934391fa9048f320409bf99cdb299
Vladyslav Yashchuk <crusader@masterofcode.com>
2015-03-29 20:07:22 +0300

    added paperclip gem to attach files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,5 @@ Rails.application.configure do
   # Advise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
