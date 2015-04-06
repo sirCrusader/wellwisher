c06889b0ffa7f56948212271f3c1120ec1fdf6b3
Alyona Dryuchilo <ADryuchilo@gmail.com>
2015-04-07 00:33:48 +0300

    edit /views/registrations file

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,38 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :lockable, :confirmable
-end
 
-def self.from_omniauth(auth)
-  find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
-end
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+    end
+    #find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
+  end
 
-def self.create_with_omniouth(auth)
-  create! do |user|
-    user.provider = auth ["provider"]
-    user.uid = auth ["uid"]
-    user.name = auth ["info"]["name"]
+  def self.create_with_omniouth(auth)
+    create! do |user|
+      user.provider = auth ["provider"]
+      user.uid = auth ["uid"]
+      user.name = auth ["info"]["name"]
     end
   end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+end
+
