d5b5bb7a9dd7f6722dd98c2a255d580e47adfb71
Vladyslav Yashchuk <crusader@masterofcode.com>
2015-04-08 22:39:19 +0300

    added actions for category controller. Added to views with forms to create and edit category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,25 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     render text: @categories.map { |i| "#{i.name}: #{i.category_image}"}.join("<br />")
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  # /categories/1/edit GET
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
