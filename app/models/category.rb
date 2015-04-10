e80d022d1b94db37ce05db0839a522b1128d648f
Vladyslav Yashchuk <crusader@masterofcode.com>
2015-04-10 22:28:57 +0300

    connect user with categories using relation one-to-many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
-  attr_accessor :name, :category_image
+  belongs_to :user
+
+  attr_accessor :name, :category_image, :user
 =begin
   has_attached_file :category_image, styles: { meduim: "300x300>", thumb: "100x100>" }
   validates_attachment :category_image, content_type: { content_type: "image/jpeg" }
