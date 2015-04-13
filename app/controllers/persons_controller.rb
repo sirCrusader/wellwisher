79793d7f6583b49460edeb691f7083f66242fb9f
mariya_martynova <comonitos_1502>
2015-04-13 18:35:04 +0300

    change persons/profile.html.erb, add elements

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -2,3 +2,25 @@ class PersonsController < ApplicationController
   def profile;
   end
 end
+
+
+
+
+# class ProfilesController < ApplicationController
+#
+#   def new
+#     @user = current_user
+#     @profile = @user.profile.new
+#   end
+#
+#   def create
+#     @user = current_user
+#     @profile = @user.profile.build(params[:profile])
+#     if @profile.save
+#       flash[:success] = "Profile Saved Successfully!"
+#       render "show"
+#     end
+#   end
+#
+#
+# end
